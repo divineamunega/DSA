0353ac3ef91e3ab38023ecab964b94319c26f722
divineamunega <divineamunega@gmail.com>
2025-01-01 15:01:52 +0100

    feat: twosum

---
+++ two_sum.js
@@ -0,0 +1,10 @@
+const twoSum = function (nums, target) {
+	for (let i = 0; i < nums.length; i++) {
+		for (let j = 0; j < nums.length; j++) {
+			if (i === j) continue;
+			if (nums[i] + nums[j] === target) return [i, j];
+		}
+	}
+};
+
+// TODO Calculate the time and space complexity
