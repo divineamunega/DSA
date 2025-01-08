53d11f8789aa739f06f14dc35376a4706d9bcdc5
divineamunega <divineamunega@gmail.com>
2025-01-08 11:57:54 +0100

    merge sort

---
+++ mergeSortedArr.js
@@ -0,0 +1,11 @@
+const merge = function (nums1, m, nums2, n) {
+	for (let i = 0; i < nums1.length; i++) {
+		if (i > m - 1) {
+			nums1[i] = nums2[i - m];
+		}
+	}
+
+	return nums1.sort((a, b) => a - b);
+};
+
+merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
